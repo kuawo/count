da33f4304a0dff789d03e94f140716eba713d11a
kuawo <63333494+kuawo@users.noreply.github.com>
2022-09-11 20:33:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,21 +8,9 @@ app.use(cors());
 
 const count_json = {}
 app.get("/api/count", async (req, res) => {
-  if (!count_json[req.query.keyword]) {
-    count_json[req.query.keyword] = 0
-  }
-  count_json[req.query.keyword] = count_json[req.query.keyword] + 1
-  res.send({ count: count_json[req.query.keyword] });
-});
-
-var vcount = 0;
-app.get("/api/vcount", async (req, res) => {
-  res.send({ data: vcount++ });
-});
-
-var mcount = 0;
-app.get("/api/mcount", async (req, res) => {
-  res.send({ data: mcount++ });
+  let key = req.query.key
+  if (!count_json[key]) { count_json[key] = 0 }
+  res.send({ count: count_json[key]++ });
 });
 
 const port = process.env.PORT || 80;
