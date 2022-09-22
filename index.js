f10c94e880a2dbfc5bb9a29306266ce61300390b
kuawo <63333494+kuawo@users.noreply.github.com>
2022-09-22 21:12:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/api/count_json_reset", async (req, res) => {
 
 const __Token = 'WILuCNR7T6FjQjH7PPUC'
 const __EncodingAESKey = 'PltVEk5gD5LuHVhfaTX9KFOd17x6BGboe5RVpJwO5jH'
-app.post("/api/kf", async (req, res) => {
+app.get("/api/kf", async (req, res) => {
   console.log(req.query)
 });
 
